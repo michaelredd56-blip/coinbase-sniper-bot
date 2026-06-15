32159c26965f3ff48362a3d1efb4571455ee68f6
michaelredd56-blip <michaelredd56@gmail.com>
2026-06-15 18:02:00 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+console.log("🚀 Coinbase Sniper Bot Started");
+
+setInterval(() => {
+    console.log("Watching BTC...");
+    console.log("Watching ETH...");
+    console.log("Watching SOL...");
+}, 10000);

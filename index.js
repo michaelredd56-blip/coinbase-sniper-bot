364a5771f93a53ae0d9e02f14ab770ca9290bf94
michaelredd56-blip <michaelredd56@gmail.com>
2026-06-18 10:00:10 -0400

    Update index.js

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const PORTFOLIO_FILE = "./portfolio.json";
 
 const CHECK_INTERVAL_SECONDS = 5;
 const TRADE_SCORE_THRESHOLD = 75;
-const TAKE_PROFIT_PERCENT = 0.004; // +0.40%
-const STOP_LOSS_PERCENT = 0.002;   // -0.20%
+const TAKE_PROFIT_PERCENT = 0.0015; // +0.15%
+const STOP_LOSS_PERCENT = 0.0010;   // -0.10%
 const MAX_OPEN_POSITIONS = 3;
 const MAX_POSITION_MINUTES = 1;
 const priceHistory = {};
